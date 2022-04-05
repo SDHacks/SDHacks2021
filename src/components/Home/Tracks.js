95e0b709389b657d91c7b6ab45c6b43fcba294b9
John Zhou <jozhou@ucsd.edu>
2022-04-05 13:05:44 -0700

    changed tracks to be kinda like figma

--- src/components/Home/Tracks.js
+++ src/components/Home/Tracks.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 
 import { ReactComponent as Learning } from '../../assets/icons/learning.svg';
 import { ReactComponent as Sustainability } from '../../assets/icons/sustainability.svg';
-import { ReactComponent as Urban } from '../../assets/icons/urban.svg';
+// import { ReactComponent as Urban } from '../../assets/icons/urban.svg';
 import { ReactComponent as Civic } from '../../assets/icons/civic.svg';
 
 // import Building from '../../assets/svg/building-jacobs.svg';
@@ -23,19 +23,7 @@ const Tracks = () => {
             name: 'health and well being',
             icon: <Learning />,
             description:
-                'In this unprecedented time, families across the United States face challenges associated with the transition to remote learning, whether it be those facing connection inequity, individuals with special needs, or people experiencing homelessness.',
-        },
-        {
-            name: 'sustainability',
-            icon: <Sustainability />,
-            description:
-                'Our climate is changing and it takes a systematic effort to combat it. This track encourages hackers to develop solutions tackling the challenges of climate change or educating the public about the environmental challenges we face.',
-        },
-        {
-            name: 'community',
-            icon: <Urban />,
-            description:
-                'In a growing society, we require new solutions tackling societal, environmental, and economic issues. This track spans everything from small neighborhood improvements to ambitious endeavors in large cities. Potential solutions include reducing carbon emissions, tackling transportation inequity, infrastructure improvements, or affordable housing.',
+                'After 2 years of ups and downs, school closures, and isolation; many of us have had our mental and overall physical health suffer greatly. Teams in this track are encouraged to create solutions that can boost any form of health, whether that be focusing on mental health or even boosting an individuals physical activity.',
         },
         {
             name: 'civic engagement',
@@ -43,6 +31,20 @@ const Tracks = () => {
             description:
                 'In a country blighted by many challenges like rising concerns of inequity, and immense social changes, there is a need for more voices to be heard. Hackers are encouraged to develop solutions to amplify the voices of everyday people.',
         },
+        {
+            name: 'sustainability',
+            icon: <Sustainability />,
+            description:
+                'Our climate is changing and it takes a systematic effort to combat it. This track encourages hackers to develop solutions tackling the challenges of climate change or educating the public about the environmental challenges we face.',
+        },
+     /*{{
+            name: 'community',
+            icon: <Urban />,
+            description:
+                'In a growing society, we require new solutions tackling societal, environmental, and economic issues. This track spans everything from small neighborhood improvements to ambitious endeavors in large cities. Potential solutions include reducing carbon emissions, tackling transportation inequity, infrastructure improvements, or affordable housing.',
+        },
+    */
+        
     ];
 
     return (
@@ -86,7 +88,7 @@ const Tracks = () => {
 };
 
 const TracksSection = styled.section`
-    background-color: var(--gray);
+    background-color: var(--primary);
     position: relative;
     padding: 10rem 0;
 
@@ -110,19 +112,19 @@ const TracksSection = styled.section`
 `;
 
 const Title = styled.h1`
-    color: var(--green);
+    color: var(--yellow-light);
     font-weight: 900;
     font-size: clamp(2rem, 15vw, 5rem);
 
     span {
-        color: var(--white);
+        color: var(--green);
         font-weight: 900;
         font-size: 1em;
     }
 `;
 
 const TracksCard = styled.div`
-    color: var(--green);
+    color: var(--purple);
     font-weight: 900;
     border: 0.5em solid;
     border-radius: 1.5em;
@@ -132,8 +134,8 @@ const TracksCard = styled.div`
     cursor: pointer;
 
     .track-num {
-        color: var(--gray);
-        background-color: var(--green);
+        color: var(--primary);
+        background-color: var(--purple);
         border-radius: 0 0.4em 0.4em 0;
         padding: 0.2em 0.5em;
         padding-bottom: 0.1em;
@@ -142,8 +144,8 @@ const TracksCard = styled.div`
     }
 
     .track-name {
-        font-size: clamp(1rem, 10vw, 3rem);
-        text-shadow: 0px 6px #201f26;
+        font-size: clamp(1rem, 10vw, 2.5rem);
+        text-shadow: 0px 4px #201f26;
 
         margin-left: 1em;
     }
